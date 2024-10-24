4cada038f52c7c87268c4e708ab20cbc6be6d3c2
RShnitser <romanshnitser@hotmail.com>
2024-10-23 20:04:17 -0400

    add unique pair constraint

--- sql/schema/003_feed_follows.sql
+++ sql/schema/003_feed_follows.sql
@@ -5,10 +5,10 @@ CREATE TABLE feed_follows (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     user_id UUID NOT NULL,
     feed_id UUID NOT NULL,
+    UNIQUE (user_id, feed_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (feed_id) REFERENCES feeds(id) ON DELETE CASCADE
 );
-
 
 -- +goose Down
 DROP TABLE feed_follows;
