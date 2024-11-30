e5322ffca74d6a8cfa784a5b123d71f8f23d893f
RShnitser <romanshnitser@hotmail.com>
2024-11-29 22:26:42 -0500

    add limit parameter

--- handler_browse.go
+++ handler_browse.go
@@ -1,5 +1,19 @@
 package main
 
-func handleBrowse(s *state, cmd command)){
+import(
+	"strconv"
+)
 
+func handleBrowse(s *state, cmd command)error{
+	limit := 2
+
+	if len(cmd.args) == 1{
+		newLimit, err := strconv.Atoi(cmd.args[0])
+		if err != nil{
+			return err
+		}
+		limit = newLimit
+	}
+
+	return nil
 }
